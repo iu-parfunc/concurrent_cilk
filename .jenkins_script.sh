efabb147b8a084c4171f0f356e7a9c4163395309
Parfunc Group Account <parfunc@indiana.edu>
2014-08-21 14:55:43 -0400

    forgot to redirect to /dev/null/ instead of file

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -6,4 +6,4 @@ source build_scripts/set_compiler
 source build_scripts/env_cilk
 export CILK_IVAR_DEBUG=false
 make
-python cilk_tests/test_cilk.py 2> gg
+python cilk_tests/test_cilk.py 2> /dev/null
