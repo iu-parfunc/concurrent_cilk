f89120f82756e0d92b2e11a3cd828799755973f7
Parfunc Group Account <parfunc@indiana.edu>
2014-08-21 15:46:27 -0400

    Now report error codes, also cho what test we are running and dir we are currently building in

--- test_cilk.py
+++ test_cilk.py
@@ -1,14 +1,20 @@
+#! /usr/bin/env python 
+
 # This script is run from CILK_ROOT
 import os
 import glob
+import sys
 
 failed = 0
 failed_tests = []
 
+failed_builds = 0
+failed_build_dirs = []
+
 for root, dirs, files in os.walk("./cilk_tests/"):
   # Don't go into .git directories
   # or the auto generated "build" dirs
-  if (".git" not in root and "build" not in root):
+  if (".git" not in root and "build" not in root and "pbbs" not in root):
     make_huh = False
     notest = False
     for file in files: 
@@ -22,8 +28,13 @@ for root, dirs, files in os.walk("./cilk_tests/"):
     cwd = os.getcwd() # get current directory
     try:
       os.chdir(root)
-      os.system("make -B") # Remake everything
+      print "BUILDING in dir: ", root
+      code = os.system("make -B") # Remake everything
+      if code != 0:
+        failed_builds += 1
+        failed_build_dirs.append(root)
       for exe in glob.glob("*.exe"):     # or in the current directory (we'll get rid of this eventually)
+        print "Running test: ", exe
         ret_code = os.system("timeout 10s " + exe)
         if ret_code != 0:
           failed += 1
@@ -37,6 +48,13 @@ for root, dirs, files in os.walk("./cilk_tests/"):
       os.chdir(cwd)
 
 print "================== Tests Finished ========================"
-print "Number of failed tests: ", failed
-print "Tests that failed     : ", failed_tests
+print "Number of failed tests  : ", failed
+print "Tests that failed       : ", failed_tests
+print "Number of failed builds : ", failed_builds
+print "Builds that failed      : ", failed_build_dirs
 print "=========================================================="
+
+# For now, don't report failed tests, but instead report failed builds
+if failed_builds != 0:
+  sys.exit(1)
+
