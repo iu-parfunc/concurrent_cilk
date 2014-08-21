9d9e7bcbc42e7a209b279fee2a16fce4a453d70a
Ryan Newton <rrnewton@gmail.com>
2014-08-21 15:08:26 -0400

    Enable output from python script

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -5,5 +5,8 @@ set -xe
 source build_scripts/set_compiler
 source build_scripts/env_cilk
 export CILK_IVAR_DEBUG=false
+
 make
-python cilk_tests/test_cilk.py 2> /dev/null
+
+python cilk_tests/test_cilk.py 
+# 2> /dev/null
