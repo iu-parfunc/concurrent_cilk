22add833be83ca85a710e49239029c1b2739b126
Parfunc Group Account <parfunc@indiana.edu>
2014-08-21 14:41:16 -0400

    Removing absolute paths, updating .jenkins file, debug prints for Ivars are now set through CILK_DEBUG_IVARS env variable. README next

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -2,5 +2,8 @@
 
 set -xe
 
-echo "Hello world: tests coming soon"
-
+source build_scripts/set_compiler
+source build_scripts/env_cilk
+export CILK_IVAR_DEBUG=false
+make
+python cilk_tests/test_cilk.py 2> gg
