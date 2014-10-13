49774f80bf7b64a3f79ca13de5ff19a6eeec9a7a
Ryan Newton <rrnewton@gmail.com>
2014-10-12 21:44:13 -0400

    Minor, print compiler version in the log

--- .jenkins_common_setup.sh
+++ .jenkins_common_setup.sh
@@ -5,5 +5,8 @@ source build_scripts/set_up_cilk_iu
 source build_scripts/env_cilk
 export CILK_IVAR_DEBUG=false
 
+echo "Using compiler, $CONCURRENTCILK_CC .  Version info:"
+$CONCURRENTCILK_CC --version
+
 # This happens in the jenkins web-based script:
 # git submodule init --update --recursive
