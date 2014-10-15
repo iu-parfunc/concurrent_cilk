798a0dffec35c08e52ce1caf7e28f4fdbe81a872
Ryan Newton <rrnewton@gmail.com>
2014-10-15 11:12:41 -0400

    Split out the microbench variants into separate benchmarks.  Update submods

--- run-benchmarks.hs
+++ run-benchmarks.hs
@@ -19,7 +19,16 @@ import System.IO.Unsafe (unsafePerformIO)
 benches :: [Benchmark DefaultParamMeaning]
 benches =
    [ (mkBenchmark "cilk_tests/parfib/Makefile"                        [ ] parfibParams     ) { progname = Just "parfib"}]            ++ 
-   [ (mkBenchmark "cilk_tests/ivar/benchmarks/microbench/Makefile"    [ ] microbenchParams ) { progname = Just "microbench"}]        ++ 
+--   [ (mkBenchmark "cilk_tests/ivar/benchmarks/microbench/Makefile"    [ ] microbenchParams ) { progname = Just "microbench"}]        ++ 
+
+   [ (mkBenchmark "cilk_tests/ivar/benchmarks/microbench/Makefile"    [ ] microbench_allblockParams ) 
+                                                                      { progname = Just "microbench_allblock"}]  ++ 
+   [ (mkBenchmark "cilk_tests/ivar/benchmarks/microbench/Makefile"    [ ] microbench_raceParams ) 
+                                                                      { progname = Just "microbench_race"}]  ++ 
+   [ (mkBenchmark "cilk_tests/ivar/benchmarks/microbench/Makefile"    [ ] microbench_noblockParams ) 
+                                                                      { progname = Just "microbench_noblock"}]  ++ 
+
+
    --[ (mkBenchmark "cilk_tests/ivar/benchmarks/wavefront/Makefile"     [ ] wavefrontParams  ) { progname = Just "wavefront"}]         ++
    [ (mkBenchmark "cilk_tests/ivar/benchmarks/pingpong/Makefile"      [ ] pingpongParams  )  { progname = Just "pingpong"}]          ++ 
  
@@ -34,21 +43,36 @@ benches =
 
 -- Set this so that HSBencher actually runs the tests that we are not passing any
 -- parameter to (at least currently)
-emptyParams      = varyCilkThreads $ Or [Set NoMeaning (CompileParam $ show 10)]
-microbenchParams = Or [ 
-                        -- varyCilkThreadsParOnly $ 
-                        -- And [ Or [ Set NoMeaning (RuntimeArg $ show (10 ^ sz) ++ " 1") | sz <- [ 0 .. 4 ] ]
-                        --     , Set (Variant "microbench_many_blocking") 
-                        --           (RuntimeEnv "VARIANT" "microbench_many_blocking") ]
-                        varyCilkThreads $ 
-                        And [ Or [ Set NoMeaning (RuntimeArg $ show (10 ^ sz)) | sz <- [ 0 .. 4 ] ]
-                            , Set (Variant var) (RuntimeEnv "VARIANT" var) ]
-                      | var <- ["microbench_race"] ]
+
+emptyParams, microbench_allblockParams, microbench_raceParams, microbench_noblockParams,
+  pingpongParams, wavefrontParams, 
+  scholesParams, choleskyParams, jacobiParams, kalahParams, knapsackParams,
+   luParams, magicNumsParams, strassenParams, parfibParams :: BenchSpace DefaultParamMeaning
+
+emptyParams      = varyCilkThreads $ Or [Set NoMeaning (CompileParam $ show (10::Int))]
+
+microbench_noblockParams  = varyThreads [2] $ mb mb_szs mb_itrs ["microbench_noblock"]
+microbench_raceParams     = varyThreads [2] $ mb mb_szs mb_itrs ["microbench_race"]
+-- Note, the allblock version spins the writer... can't run on 1 thread:
+microbench_allblockParams = varyThreads [2] $ mb mb_szs mb_itrs ["microbench_allblock"]
+
+mb_szs :: [Int]
+mb_szs  = (map (10^) [ 0 .. 4::Int ])
+
+mb_itrs :: [Int]
+mb_itrs = (map (10^) [ 0 .. 3::Int ])
+
+mb :: [Int] -> [Int] -> [String] -> BenchSpace DefaultParamMeaning
+mb fbrs iters vars = 
+  Or [ And [ Set NoMeaning (RuntimeArg $ unwords [show fbr, show iter])
+           , Set (Variant var) (RuntimeEnv "VARIANT" var) ]
+     | var <- vars, fbr <- fbrs, iter <- iters ]
+----------------------------------------
 
 pingpongParams  = varyCilkThreads $ 
                    And [ Or [ Set NoMeaning (RuntimeArg $ unwords [show pairs, show iters]) 
-                            | pairs <- [ 1, 2, 4, 8 ]
-                            , iters <- [ 100, 500, 1000 ] ]
+                            | pairs <- [ 1, 2, 4, 8 ]     :: [Int]
+                            , iters <- [ 100, 500, 1000 ] :: [Int] ]
                        , Set (NoMeaning) (RuntimeEnv "VARIANT" "pingpong_ivars") ] 
 
 wavefrontParams  = varyCilkThreads emptyParams
@@ -65,6 +89,7 @@ parfibParams     = varyCilkThreads $
                        -- These are running only on MUCH smaller sizes:
                        , pfibs [10, 11, 12, 13] ["fib_pthread"] ]
  where 
+   pfibs :: [Int] -> [String] -> BenchSpace DefaultParamMeaning
    pfibs szs vars = 
      And [ Or [ Set NoMeaning (RuntimeArg $ show sz) | sz <- szs ]
          , Or [ Set (Variant var) (RuntimeEnv "PARFIB_VARIANT" var) | var <- vars ] ]
@@ -72,16 +97,16 @@ parfibParams     = varyCilkThreads $
 
 -- | GHC specific method of varying threads.
 varyCilkThreads :: BenchSpace DefaultParamMeaning -> BenchSpace DefaultParamMeaning
-varyCilkThreads conf = And [ conf, Or (map fn threadSelection) ]
- where
-   fn n = Set (Threads n) $ RuntimeEnv "CILK_NWORKERS" (show n)
+varyCilkThreads = varyThreads threadSelection
 
 -- | Skip the one-thread version.
 varyCilkThreadsParOnly :: BenchSpace DefaultParamMeaning -> BenchSpace DefaultParamMeaning
-varyCilkThreadsParOnly conf = And [ conf, Or (map fn (filter (>1) threadSelection)) ]
+varyCilkThreadsParOnly = varyThreads (filter (>1) threadSelection)
+
+varyThreads :: [Int] -> BenchSpace DefaultParamMeaning -> BenchSpace DefaultParamMeaning
+varyThreads threads conf = And [ conf, Or (map fn threads) ]
  where
    fn n = Set (Threads n) $ RuntimeEnv "CILK_NWORKERS" (show n)
-
 
 -- | Default threading settings based on the number of processors on the current machine.
 threadSelection :: [Int]
