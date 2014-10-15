abf5bd16e4df9a2dfa59c49b66b5e0118b335315
Ryan Newton <rrnewton@gmail.com>
2014-10-15 12:55:45 -0400

    Allow runbenchmark script to run without rootdir argument

--- .run-benchmarks.sh
+++ .run-benchmarks.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 # Script used by Jenkins to run benchmarks.
 
 echo "Run benchmarks script starting, located at: $0"
 echo "Run on machine $HOSTNAME ... SHELL=$SHELL LANG=$LANG"
 echo "Full locale is:"
-locale
+locale || echo ok
 
-rootdir=$1
-shift
+if [ "$rootdir" == "" ]; then
+  rootdir=`pwd`
+else
+  rootdir=$1
+  shift
+fi
 export BENCHARGS=$*
 echo "Read rootdir from first argument: $rootdir"
 
-if [ "$rootdir" == "" ] || ! [ -d "$rootdir" ]; 
+if ! [ -d "$rootdir" ]; 
 then echo ".run-benchmarks, cannot proceed because rootdir ($rootdir) does not exist."
      exit 1 
 fi 
