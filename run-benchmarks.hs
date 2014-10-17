e2ae0b5c0462f7ea92b81184ac956e9596299b8a
Ryan Newton <rrnewton@gmail.com>
2014-10-17 16:53:15 -0400

    tweak the bench harness to listen for two more outputs from the dump_stats

--- run-benchmarks.hs
+++ run-benchmarks.hs
@@ -207,6 +207,8 @@ main = do
                        customTagHarvesterInt "CILKPLUS_RUNTIME_MEMORY_USAGE_BYTES" `mappend` 
                        customTagHarvesterInt "CILKPLUS_STACKSIZE" `mappend` 
                        customTagHarvesterInt "CILKPLUS_TOTALSTACKS" `mappend` 
+                       customTagHarvesterInt "CCILK_TOTAL_STACKS_ADDED" `mappend` 
+                       customTagHarvesterInt "CCILK_TOTAL_PAUSE_EVENTS" `mappend` 
                        customTagHarvesterInt "CONCURRENTCILK_WORKERS_BLOCKED" `mappend` 
                        harvesters conf
         , systemCleaner = Cleanup $ do 
