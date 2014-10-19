f0aadd97543a5441ff4e7ebd03e219968bbee517
Christopher Zakian <czakian@gmail.com>
2014-10-18 21:53:05 -0700

    remove spurious locked state in writer

--- runtime/ivar_full_blocking.c
+++ runtime/ivar_full_blocking.c
@@ -14,16 +14,16 @@ void initialize_stack_queue(queue_t *q);
 void clear_stack_queue(queue_t* q);
 
 inline CILK_API(void)
-__cilkrts_ivar_clear(__cilkrts_ivar* ivar) { *ivar = 0; __sync_synchronize(); }
+  __cilkrts_ivar_clear(__cilkrts_ivar* ivar) { *ivar = 0; __sync_synchronize(); }
 
-inline CILK_API(void)
+  inline CILK_API(void)
 __cilkrts_ivar_array_clear(__cilkrts_ivar *ptr, int size)
 { 
   memset(ptr, 0, sizeof(__cilkrts_ivar) * (size_t) size);
   __sync_synchronize();
 }
 
-inline CILK_API(__cilkrts_ivar*)
+  inline CILK_API(__cilkrts_ivar*)
 __cilkrts_new_ivar_array(int size)
 {
   __cilkrts_ivar* ptr = calloc(sizeof(__cilkrts_ivar), size);
@@ -56,7 +56,7 @@ typedef struct cons cons_t;
  *         -- pause the continuation and steal work.
  *         -- once the IVar is full, the function will return the value.
  */
-inline CILK_API(ivar_payload_t)
+  inline CILK_API(ivar_payload_t)
 __cilkrts_ivar_read(__cilkrts_ivar *ivar)
 {
   __cilkrts_worker *w, *replacement;
@@ -102,7 +102,7 @@ __cilkrts_ivar_read(__cilkrts_ivar *ivar)
         case CILK_IVAR_PAUSED:
           cur_cell = (cons_t *)(peek >> IVAR_SHIFT);
           CILK_ASSERT(cur_cell); // Never empty because it starts as a singleton
-	  my_waitlist_cell.cdr = cur_cell; // If we bump them with CAS, then they are our neighor.
+          my_waitlist_cell.cdr = cur_cell; // If we bump them with CAS, then they are our neighor.
           exit = cas(ivar, peek, my_payload);
           if (exit) dbgprint(IVAR,"ivar %p PAUSED. added worker %p to waitlist %p\n",ivar,w,waitlist);
           break;
@@ -171,8 +171,6 @@ __cilkrts_ivar_write(__cilkrts_ivar *ivar, ivar_payload_t val)
         dbgprint(IVAR, "old_val 0x%lx flags 0x%lx\n", ((uintptr_t) old_val >> IVAR_SHIFT), old_val & IVAR_MASK);
         if ((*ivar & IVAR_MASK) == CILK_IVAR_FULL)  { exit = 1; }
         break;  
-      case CILK_IVAR_LOCKED:
-        break; //go around again
       case CILK_IVAR_FULL:
         __cilkrts_bug("Attempted multiple puts on Cilk IVar %p. Aborting program.\n", ivar);
       default:
